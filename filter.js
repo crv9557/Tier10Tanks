f38b2140f7ee8ba56966a65f50c296965e8b07bd
Connor Ring <crv9557@saintjoe.edu>
2015-03-01 23:53:17 -0500

    Fixed a few bugs, and got the filter working

--- filter.js
+++ filter.js
@@ -1,17 +1,18 @@
 //Connor Ring
 //CMP237 Filter File for Tanks
 // This is meant to find any tank between 50 and 60 in the "Weight" data and print them.
-load('tankTraits.js);
+// Edit (3/1/2015): Fixed a slight bug with "load". Fixed indentation. 
+load('tankTraits.js');
 var tanks = JSON.parse(Tanks);
 function filter(array, test) {
-	  var passed = [];
-	    for (var i = 0; i < array.length; i++) {
-		        if (test(array[i]))
-				      passed.push(array[i]);
-			  }
-	      return passed;
+  var passed = [];
+  for (var i = 0; i < array.length; i++) {
+    if (test(array[i]))
+      passed.push(array[i]);
+  }
+  return passed;
 }
 
 print(JSON.stringify(filter(tanks, function(tanks) {
-	  return tanks.Weight > 50 && tanks.Weight < 60 ;
+  return tanks.Weight > 50 && tanks.Weight < 52;
 })));
