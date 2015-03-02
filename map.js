6e4a347954c99fc398233058ef592c3decc16596
Connor Ring <crv9557@saintjoe.edu>
2015-03-02 00:24:34 -0500

    Fixed a few bugs, added more readability, and got the whole portion to work properly.

--- map.js
+++ map.js
@@ -1,15 +1,20 @@
 //Connor Ring
 ////CMP237 Mapping Arrangement
 ////This is meant to be used for updating the data of any given tank
+//Edit (3/1/2015): Added the import of data. Fixed a very odd bug that involved the letter "i" being in the middle of nowhere.
+var data = require('./tankTraits.js');
+var Tanks = JSON.parse(data);
+
 function map(array, transform) {
-          var mapped = [];
-  i           for (var i = 0; i < array.length; i++)
-                        mapped.push(transform(array[i]));
-              return mapped;
+  var mapped = [];
+  for (var i = 0; i < array.length; i++)
+    mapped.push(transform(array[i]));
+    return mapped;
 }
+
 var overweight = Tanks.filter(function(tank) {
-          return Tanks.Weight - Tanks.Weight > 90;
+  return tank.Weight > 90 && tank.Weight < 150;
 });
 console.log(map(overweight, function(tank) {
-          return Tanks.Name;
+  return tank.Name + " :  " + tank.Weight + " tons";
 }));
