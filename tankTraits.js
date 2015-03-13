f130e34e7de41d97ad242d000387e746c2442c1c
Connor Ring <crv9557@saintjoe.edu>
2015-03-13 01:23:09 -0400

    Committing for hopeful fix

--- tankTraits.js
+++ tankTraits.js
@@ -9,16 +9,16 @@
 //Edit (3/1/2015): Removed text values in the numerical value areas. Changed values to be average rather than lowest to highest where applicable. Also changed value "Engine" to "Top Speed" for clarity.
 
 // Create an array for the tanks and their properties.
-var Tanks = [ "[\n  " + [
-'{"Name":"T92","Damage":"2250","Penetration:"120","Top Speed":"32","Weight":"57.84"}',
+var Tanks = "[\n  " + [
+'{"Name":"T92","Damage":"2250","Penetration":"120","Top Speed":"32","Weight":"57.84"}',
 '{"Name":"T57 Heavy","Damage":"400","Penetration":"258","Top Speed":"35","Weight":"54.93"}',
 '{"Name":"T110E4","Damage":"750","Penetration":"295","Top Speed":"35","Weight":"64.73"}',
 '{"Name":"T110E3","Damage":"750","Penetration":"295","Top Speed":"24","Weight":"76.73"}',
 '{"Name":"M48A1 Patton","Damage":"390","Penetration":"268","Top Speed":"45","Weight":"48.17"}',
-'{"Name":"T110E5","Damage":"400",Penetration:"258","Top Speed":"37","Weight":"53.76"}',
-'{"Name":"FV4202","Damage":"390",Penetration:"268","Top Speed":"40","Weight":"40"}',
-'{"Name":"FV215b (183)","Damage":"1150",Penetration:"310","Top Speed":"34","Weight":"60"}',
-'{"Name":"FV215b","Damage":"400",Penetration:"259","Top Speed":"34","Weight":"70"}',
+'{"Name":"T110E5","Damage":"400","Penetration":"258","Top Speed":"37","Weight":"53.76"}',
+'{"Name":"FV4202","Damage":"390","Penetration":"268","Top Speed":"40","Weight":"40"}',
+'{"Name":"FV215b (183)","Damage":"1150","Penetration":"310","Top Speed":"34","Weight":"60"}',
+'{"Name":"FV215b","Damage":"400","Penetration":"259","Top Speed":"34","Weight":"70"}',
 '{"Name":"FV4005 Stage II","Damage":"1150 HP","Penetration":"310","Top Speed":"35","Weight":"50.8"}',
 '{"Name":"Conqueror Gun Carriage","Damage":"2200","Penetration":"117","Top Speed":"34","Weight":"55"}',
 '{"Name":"G.W.E 100","Damage":"2000","Penetration":"105","Top Speed":"40","Weight":"87"}',
@@ -34,14 +34,14 @@ var Tanks = [ "[\n  " + [
 '{"Name":"Bat.-Chatillon 155 58","Damage":"1250","Penetration":"95","Top Speed":"62","Weight":"35"}',
 '{"Name":"Object 268","Damage":"750","Penetration":"303","Top Speed":"48","Weight":"50.97"}',
 '{"Name":"Object 263","Damage":"560","Penetration":"295","Top Speed":"55","Weight":"60.01"}',
-'{"Name":"Object 430","Damage":"320",Penetration:"264","Top Speed":"55","Weight":"35.3"}',
+'{"Name":"Object 430","Damage":"320","Penetration":"264","Top Speed":"55","Weight":"35.3"}',
 '{"Name":"T-62A","Damage":"320","Penetration":"264","Top Speed":"50","Weight":"37"}',
 '{"Name":"Object 140","Damage":"320","Penetration":"","Top Speed":"55","Weight":"36"}',
-'{"Name":"IS-7","Damage":"490",Penetration:"250","Top Speed":"59.6","Weight":"68.19"}',
+'{"Name":"IS-7","Damage":"490","Penetration":"250","Top Speed":"59.6","Weight":"68.19"}',
 '{"Name":"IS-4","Damage":"440","Penetration":"258","Top Speed":"43","Weight":"60.02"}',
 '{"Name":"Object 261","Damage":"1700","Penetration":"102","Top Speed":"50","Weight":"59.1"}',
 '{"Name":"121","Damage":"440","Penetration":"258","Top Speed":"56","Weight":"39"}',
-'{"Name":"113","Damage":"440",Penetration:"249","Top Speed":"50","Weight":"45"}',
+'{"Name":"113","Damage":"440","Penetration":"249","Top Speed":"50","Weight":"45"}',
 '{"Name":"STB-1","Damage":"390","Penetration":"258","Top Speed":"53","Weight":"37.4"}'
 ].join(",\n  ") + "\n]";
 
