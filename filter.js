0b1e1e011fc35e746e976c9cbe83cf60ddbf059a
Connor Ring <crv9557@saintjoe.edu>
2015-03-13 01:24:30 -0400

    Committing for hopeful fix (again)

--- filter.js
+++ filter.js
@@ -1,8 +1,8 @@
 //Connor Ring
 //CMP237 Filter File for Tanks
-// This is meant to find any tank between 50 and 60 in the "Weight" data and pri
-nt them.
+// This is meant to find any tank between 50 and 60 in the "Weight" data and print them
 // Edit (3/1/2015): Fixed a slight bug with "load". Fixed indentation.
+// Edit (3/13/2015): WOW! Fixed a GLARING bug that was caused by improper use of "//".
 load('tankTraits.js');
 var tanks = JSON.parse(Tanks);
 function filter(array, test) {
